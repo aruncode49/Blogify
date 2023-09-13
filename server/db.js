e57cd66ce1eb52119416a3bc29ba2a12b46b91df
Arun Kumar <aruncode49@gmail.com>
2023-09-13 16:51:28 +0530

    configured dotenv file

--- server/db.js
+++ server/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const { MONGODB_URI } = require("../config");
 
 function connectMongoDB() {
   mongoose
-    .connect(MONGODB_URI)
+    .connect(process.env.MONGODB_URI)
     .then((val) => console.log("MongoDB Connected"))
     .catch((err) => console.log("Error in connection: ", err));
 }
